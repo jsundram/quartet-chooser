3afdbd6b2867fdaf7636d86475e4da4f9b684e2d
Jason Sundram <jsundram@gmail.com>
2022-09-07 17:12:00 -0400

    formatting

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,49 +1,49 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: `https://quartetroulette.com`,
-    title: "Quartet Roulette",
-  },
-  plugins: [
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "G-7TZZMJ8149",
-      },
+    siteMetadata: {
+        siteUrl: `https://quartetroulette.com`,
+        title: "Quartet Roulette",
     },
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "./static/icon.png",
-      },
-    },
-    {
-      resolve: "gatsby-plugin-sitemap",
-      options: {
-        resolveSiteUrl: () => `https://quartetroulette.com`,  // not sure why this is needed
-        query: `
-        query MyQuery {
-          allSitePage {
-            nodes {
-              path
-            }
-          }
-        }`,
-      },
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-    `gatsby-transformer-json`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `./src/data/`,
-      },
-    },
-  ],
+    plugins: [
+        {
+            resolve: "gatsby-plugin-google-analytics",
+            options: {
+                trackingId: "G-7TZZMJ8149",
+            },
+        },
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                icon: "./static/icon.png",
+            },
+        },
+        {
+            resolve: "gatsby-plugin-sitemap",
+            options: {
+                resolveSiteUrl: () => `https://quartetroulette.com`,  // not sure why this is needed
+                query: `
+                    query MyQuery {
+                        allSitePage {
+                            nodes {
+                                path
+                            }
+                        }
+                    }`,
+            },
+        },
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "pages",
+                path: "./src/pages/",
+            },
+            __key: "pages",
+        },
+        `gatsby-transformer-json`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `./src/data/`,
+            },
+        },
+    ],
 };
